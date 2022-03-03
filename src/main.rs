6fbdd6661308643b16b8bf3d2ba3486513a32499
Andrés Estrella <andresmauricioestrella@gmail.com>
2022-03-02 21:26:51 -0400

    Done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+extern crate tokens_lib;
+use tokens_lib::*;
 use std::{env, io::{BufReader, self, prelude::*}, slice::SliceIndex, collections::HashMap, ops::Add};
 use std::fs::File;
 
@@ -6,27 +8,19 @@ pub struct ItemSetData{
     pub candidates:Vec<String>,
     pub itemSet:Vec<String>,
     pub finalFrequentItemSet:Vec<String>,
-    pub frequentItems:HashMap<String,i32>,
-    pub itemsCount:usize, countItemOccurrence:i32, displayFrequentItemSetNumber:i32, minsup:i32,minconf:i32
+    pub frequentItems:HashMap<String,usize>,
+    pub itemsCount:usize, countItemOccurrence:usize, displayFrequentItemSetNumber:usize, 
+    pub minsup:usize,
+    pub minconf:f32
 }
 
 fn main() -> io::Result<()> {
 
     // how can i error handle missing arguments here?
     let args: Vec<String> = env::args().collect(); //arg 1 is minsup and arg 2 is min confidence
-    let fname = &args[1]; //.unwrap()
-    let _min_sup:f32 = args[2].parse().expect("missing min supp argument"); //.unwrap()
-    let _min_conf:f32 = args[3].parse().expect("missing min confidence argument");
-
+    let fname = &args[1]; //args[0] is already reserved for something
     let file = File::open(fname)?;
     let reader = BufReader::new(file);
-
-    //find a way to declare vec and collect lines in a single line
-    //let transactions:Vec<String> = reader.lines().collect::<Result<_,_>>().unwrap();
-    //let no_of_transactions = transactions.len();
-    //let itemSet:Vec<String> = Vec::new();
-    //let finalFrequentItemSet:Vec<String> = Vec::new();
-    //let mut frequentItems:HashMap<String,i32> = HashMap::new();
 
     let mut appData = ItemSetData{
         transactions: reader.lines().collect::<Result<_,_>>().unwrap(),
@@ -46,12 +40,11 @@ fn main() -> io::Result<()> {
         transactions.push(line.unwrap());
     } */
 
-    //display(transactions, no_of_transactions,_min_sup,_min_conf);
     display(&mut appData);
     Ok(())
 }
 
-//fn display (transactions:Vec<String>, no_of_transactions:usize, minsup:f32, minconf:f32){
+
 fn display (data: &mut ItemSetData){
     for n in 0..data.transactions.len(){
 
@@ -63,29 +56,26 @@ fn display (data: &mut ItemSetData){
                     data.itemSet.push(word.to_string())
                 }
             }
+            
         }
         /* let string: String = data.transactions.get(n).unwrap().to_string();
-        let words:Vec<String> = string.split_whitespace().map(|x| x.to_string()).collect::<Vec<String>>();
-        print!("{:?} 3 \n", words); */
+        let words:Vec<String> = string.split_whitespace().map(|x| x.to_string()).collect::<Vec<String>>();*/
     }
     data.itemsCount = data.itemSet.len();
     data.itemSet.sort();
-    println!("Item set: {:?}, delete this bruh line 69", data.itemSet);
-    println!("Number of items{}", data.itemsCount);
-    println!("Number of transactions{}", data.transactions.len());
+    println!("Item set: {:?}", data.itemSet);
+    println!("Number of items: {}", data.itemsCount);
+    println!("Number of transactions: {}", data.transactions.len());
 
     firstFrequentItemSet(data);
 }
 
 fn firstFrequentItemSet(data:&mut ItemSetData){
-    println!("Frequent ItemSet 1");
+
     for item in &data.itemSet{
         data.countItemOccurrence = 0;
-        //let itemStr = data.itemSet.get(i).unwrap();
 
-        for j in 0..data.transactions.len(){
-
-            let transactionStr = data.transactions.get(j).unwrap();
+        for transactionStr in &data.transactions{
 
             if transactionStr.contains(item){
                 data.countItemOccurrence+=1;
@@ -94,22 +84,22 @@ fn firstFrequentItemSet(data:&mut ItemSetData){
         }
 
         if data.countItemOccurrence >= data.minsup{
-            println!();
-            data.finalFrequentItemSet.push(item.clone());
+            println!("{} => support = {}", item, data.countItemOccurrence);
+            data.finalFrequentItemSet.push(item.to_string());
             data.frequentItems.insert(item.to_string(), data.countItemOccurrence);
         }
-
     }
     aprioriStart(data);
+}
 
-fn aprioriStart(data:&mut ItemSetData){}
+fn aprioriStart(data:&mut ItemSetData){
     let mut itemSetNumber = 1;
 
     for set in &data.finalFrequentItemSet{
         data.candidates.push(set.to_string());
     }
 
-    loop{
+    while data.candidates.len() > 1{
         itemSetNumber += 1;
         generateCombinations(data, itemSetNumber);
         checkFrequentItems(data);
@@ -119,55 +109,150 @@ fn aprioriStart(data:&mut ItemSetData){}
         }
     }
 
-    println!("Association Rules for Frequent Itemset");
+    println!("\nAssociation Rules for Frequent Itemset");
     generateAssociationRules(data);
 }
+    
+
 
 fn generateCombinations(data: &mut ItemSetData,mut itr:i32){
-    let candidatesTemp:Vec<String> = Vec::new();
-    let s1:String;
-    let s2:String;
-    //StringTokenizers 1 y 2
-
+    let mut candidatesTemp:Vec<String> = Vec::new();
+    let mut s1:String;
+    let mut s2:String;
+   
 
     if itr==2{
         for i in 0..data.candidates.len(){
-            //strToken1 =
-            //s1 = strToken1.next();
+
+            let mut strTokenizer1 = FilteredTokenizer::new(filters::DefaultFilter{}, data.candidates.get(i).unwrap());
+            s1 = strTokenizer1.next().unwrap().term().to_string();
+            
             for j in i+1..data.candidates.len(){
-                //strToken2 = new ;
-				//s2 = strToken2.nextToken();
-                let addString:String = String::from(&s1.to_string()+" "+&s2.to_string());
+                let mut strTokenizer2 = FilteredTokenizer::new(filters::DefaultFilter{}, data.candidates.get(j).unwrap());
+				s2 = strTokenizer2.next().unwrap().term().to_string();
+                let addString:String = String::from(s1.to_string() + " " + &s2.to_string());
                 candidatesTemp.push(addString);
             }  
         }
     }else{
         for i in 0..data.candidates.len(){
             for j in i+1..data.candidates.len(){
-
                 s1 = String::new();
                 s2 = String::new();
 
-                //strToken1 = ;
-                //strToken2 = ;
+                let mut strTokenizer1 = FilteredTokenizer::new(filters::DefaultFilter{}, data.candidates.get(i).unwrap());
+                let mut strTokenizer2 = FilteredTokenizer::new(filters::DefaultFilter{}, data.candidates.get(j).unwrap());
 
                 for s in 0..itr-2{
-                    //s1 = s1 + " " + strToken.nextToken();
-                    //s2 = s2 + " " + strToken2.nextToken();
+                    s1 = s1 + " " + &strTokenizer1.next().unwrap().term().to_string();
+                    s2 = s2 + " " + &strTokenizer2.next().unwrap().term().to_string();
                 }
 
-                if !s2.eq_ignore_ascii_case(&s1){ //=0 for false i guess
-                    //let addString:String = (s1 + " " + strToken1.nextToken() + " " + strToken2.nextToken()).trim();
+                if s2.eq_ignore_ascii_case(&s1){
+                    let addString:String = (s1 + " " + &strTokenizer1.next().unwrap().term().to_string() + " " + &strTokenizer2.next().unwrap().term().to_string()).trim().to_string();
                     candidatesTemp.push(addString);
                 }
-                
-
             }  
         }
-
     }
+    data.candidates.clear();
+    data.candidates = candidatesTemp.clone(); 
+    candidatesTemp.clear();
 }
 
-fn checkFrequentItems(data:&mut ItemSetData){}
+fn checkFrequentItems(data:&mut ItemSetData){
 
-fn generateAssociationRules(data:&mut ItemSetData){}  
+    let mut combList:Vec<String> = Vec::new();
+    for candidate in &data.candidates{
+        combList.push(candidate.to_string())
+    } 
+    println!("Frequent Itemset: {}", data.displayFrequentItemSetNumber);
+
+    for str in combList{
+        let mut flag = 0;
+        let mut itemSetOcurrence:usize = 0;
+        let words:Vec<String> = str.split_whitespace().map(|x| x.to_string()).collect::<Vec<String>>();
+        let count = words.len();
+
+        for transaction in &data.transactions{ 
+
+            for wordStr in &words{ 
+                if transaction.contains(wordStr){
+                    flag+=1;
+                }
+            }
+            if flag==count{
+                itemSetOcurrence+=1;
+            }
+            flag=0;
+        }
+
+        if itemSetOcurrence >= data.minsup{
+            println!("{} => support = {}", str, itemSetOcurrence);
+            data.frequentItems.insert(str.clone(), itemSetOcurrence);
+            data.finalFrequentItemSet.push(str.clone());
+        }
+        itemSetOcurrence = 0;
+    }
+    data.displayFrequentItemSetNumber += 1;
+
+}
+
+fn generateAssociationRules(data:&mut ItemSetData){
+    let mut confidence:f32;
+    let mut confidence2:f32;
+
+    for item in &data.finalFrequentItemSet{ 
+        let value = data.frequentItems.get(item).unwrap();
+        let mut str:String = String::new();
+        let mut str1:String = String::new();
+        let words:Vec<String> = item.split_whitespace().map(|x| x.to_string()).collect::<Vec<String>>();
+        let wordCountInString = words.len();
+
+        if wordCountInString == 2{
+            let s = data.frequentItems.get(&words[0]).unwrap();
+            confidence = (*value as f32/ *s as f32);
+
+            if confidence>=data.minconf{
+                println!("{} -> {} = Confidence = {} and Support = {}", words[0], words[1], confidence*100 as f32, value); 
+            }
+            let s1 = data.frequentItems.get(&words[1]).unwrap();
+            confidence = (*value as f32/ *s1 as f32);
+
+            if confidence>=data.minconf{
+                println!("{} -> {} = Confidence = {} and Support = {}", words[1], words[0], confidence*100 as f32, value);
+            }
+            
+        }else{
+            for i in 0..wordCountInString-1{
+
+                if i == 0{
+                    str = str + &words[i];
+                }else{
+                    str = str + " " + &words[i];
+                }
+
+                for j in i+1..wordCountInString{
+                    str1 = str1 + " " + &words[j];
+                }
+                let s = data.frequentItems.get(&str).unwrap();
+                confidence = (*value as f32/ *s as f32);
+                let st:String = str1.trim().to_string();
+                let s1 = data.frequentItems.get(&st).unwrap();
+                confidence2 = (*value as f32/ *s1 as f32);
+
+                if confidence >= data.minconf{
+                println!("{} -> {} = Comfidemce = {} and Support = {}", str, str1, confidence*100 as f32, value);
+                }
+
+                if confidence2 >= data.minconf{
+                println!("{} -> {} = Comfidemce = {} and Support = {}", st, str, confidence2*100 as f32, value);
+                }
+        
+            str1="".to_string();   
+            }
+            str="".to_string();
+            str1="".to_string();  
+        }
+    }
+}  
